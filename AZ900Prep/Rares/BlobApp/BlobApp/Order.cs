2878ec6ad16cf3d906acc4e7cce167aacc66c911
Rares Manguci <raresmanguci@outlook.com>
2023-08-28 17:20:43 +0300

    Blobs and Table Storage

---
+++ AZ900Prep/Rares/BlobApp/BlobApp/Order.cs
@@ -0,0 +1,17 @@
+﻿using Azure.Data.Tables;
+using Azure;
+
+namespace BlobApp
+{
+    public class Order : ITableEntity
+    {
+        public string orderId { get; set; }
+        public string category { get; set; }
+
+        public int quantity { get; set; }
+        public string PartitionKey { get; set; }
+        public string RowKey { get; set; }
+        public DateTimeOffset? Timestamp { get; set; }
+        public ETag ETag { get; set; }
+    }
+}
--- AZ900Prep/Rares/BlobApp/BlobApp/Program.cs
+++ AZ900Prep/Rares/BlobApp/BlobApp/Program.cs
@@ -1,26 +1,9 @@
-﻿using Azure.Storage.Blobs;
-using System.ComponentModel;
+﻿using BlobApp;
 
-string connectionString = "DefaultEndpointsProtocol=https;AccountName=az204bd17;AccountKey=M14U6hJmEbHxuBZ7XuMPpiQDz9w9fegEwD95ux2jAYht8iuySlNwwyA9+trnAP4e+ki35DzTi5Ia+AStrU4Q0w==;EndpointSuffix=core.windows.net";
-string containerName = "data1";
-var blobName = "journal.txt";
-var filePath = "C:\\temp\\journal.txt";
+//await new Blob().HandleBlobs();
+var table = new TableStorage();
+table.AddEntities();
+table.UpdateEntity("Mobile", "O1", 69);
+table.QueryEntity("Mobile");
 
-BlobServiceClient blobServiceClient = new BlobServiceClient(connectionString);
-
-Console.WriteLine("Creating the container");
-
-var container = blobServiceClient.GetBlobContainerClient(containerName);
-container.CreateIfNotExists();
-
-
-///*
-//If you want to specify properties for the container
-
-//await blobServiceClient.CreateBlobContainerAsync(containerName,Azure.Storage.Blobs.Models.PublicAccessType.Blob);
-//*/
-//Console.WriteLine("Container creation complete");
-
-BlobContainerClient blobContainerClient = new BlobContainerClient(connectionString, containerName);
-BlobClient blobClient = blobContainerClient.GetBlobClient(blobName);
-await blobClient.UploadAsync(filePath, true);
+table.GetEntityAndDeleteThem();
