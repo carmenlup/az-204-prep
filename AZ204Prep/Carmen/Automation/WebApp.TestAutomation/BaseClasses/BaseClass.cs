db03830f1fccf8d1f614e3fea908b791b88d5294
Carmen-Gabriela Lup <carmen.gabriela.lup@gmail.com>
2023-12-13 16:15:26 +0200

    add development done by QA to the  new solution

---
+++ AZ204Prep/Carmen/Automation/WebApp.TestAutomation/BaseClasses/BaseClass.cs
@@ -0,0 +1,59 @@
+﻿using Automation.Config;
+using Automation.CustomException;
+using Automation.Settings;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Automation.BaseClasses
+{
+    [TestClass]
+    public class BaseClass
+    {
+        private static IWebDriver GetChromeDriver()
+        {
+            IWebDriver driver = new ChromeDriver();
+            return driver;
+        }
+
+        private static IWebDriver GetFireFoxDriver()
+        {
+            IWebDriver driver = new FirefoxDriver();
+            return driver;
+        }
+
+        [AssemblyInitialize]
+        public static void InitWebDriver(TestContext tc)
+        {
+            ObjectRepository.Config = new AppConfigReader();
+
+            switch (ObjectRepository.Config.GetBrowser())
+            {
+                case BrowserType.Chrome:
+                    ObjectRepository.Driver = GetChromeDriver();
+                    break;
+                case BrowserType.Firefox:
+                    ObjectRepository.Driver = GetFireFoxDriver();
+                    break;
+                default:
+                    throw new NoDriverFound("Driver not found: " + ObjectRepository.Config.GetBrowser().ToString());
+            }
+        }
+
+        [AssemblyCleanup]
+        public static void TearDown()
+        {
+            if (ObjectRepository.Driver != null)
+            {
+                ObjectRepository.Driver.Close();
+                ObjectRepository.Driver.Quit();
+            }
+        }   
+    }
+}
