ef4742b4784bc6fa65cedf4d74e77c172c09ac01
Carmen Gabriela Lup <carmen.gabriela.lup@gmail.com>
2024-02-12 21:18:06 +0200

    add configuration files to function app

--- AZ204Prep/Carmen/SqlFunction/Startup.cs
+++ AZ204Prep/Carmen/SqlFunction/Startup.cs
@@ -1,8 +1,10 @@
 ﻿//using Microsoft.Azure.Functions.Extensions.DependencyInjection;
 using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Http;
 using SqlFunction.Services;
+using System.IO;
 using System.Xml.Linq;
 
 [assembly: FunctionsStartup(typeof(SqlFunction.Startup))]
@@ -11,8 +13,20 @@ namespace SqlFunction;
 
 public class Startup : FunctionsStartup
 {
+    public override void ConfigureAppConfiguration(IFunctionsConfigurationBuilder builder)
+    {
+        FunctionsHostBuilderContext context = builder.GetContext();
+
+        builder.ConfigurationBuilder
+            .AddJsonFile(Path.Combine(context.ApplicationRootPath, "appsettings.json"), optional: true, reloadOnChange: false)
+            .AddJsonFile(Path.Combine(context.ApplicationRootPath, $"appsettings.{context.EnvironmentName}.json"), optional: true, reloadOnChange: false)
+            .AddEnvironmentVariables();
+    }
+
     public override void Configure(IFunctionsHostBuilder builder)
     {
+        FunctionsHostBuilderContext context = builder.GetContext();
+        
         builder.Services.AddHttpClient();
 
         builder.Services.AddSingleton<IDbService>((s) =>
